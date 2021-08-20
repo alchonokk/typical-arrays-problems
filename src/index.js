b4b1576dfb6fdbdea3b923cb45287fc4a30a1ed5
alchonokk <galunia_suglob@mail.ru>
2021-08-20 23:15:27 +0300

    feat: add check zero array

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-
-exports.min = function min (array) {
-  
+if (array=== undefined || array.length===0){
   return 0;
+}
+exports.min = function min (array) {
+  if (array=== undefined || array.length===0){
+    return 0;
+  }
+  
+
 
 
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array=== undefined || array.length===0){
+    return 0;
+  }
+
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array=== undefined || array.length===0){
+    return 0;
+  }
+
+
+  
 }
